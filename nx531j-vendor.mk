9b31f9dc02dec563a7c4595d34d77e7965b59435
XiNGRZ <chenxingyu92@gmail.com>
2016-09-05 02:02:36 +0800

    nx531j: regenerate without display hal
    Change-Id: If7c3fce17472d8e90a4f7d4e494d5b2e87732702

--- nx531j-vendor.mk
+++ nx531j-vendor.mk
@@ -126,7 +126,6 @@ PRODUCT_COPY_FILES += \
     vendor/nubia/nx531j/proprietary/framework/org.codeaurora.camera.jar:system/framework/org.codeaurora.camera.jar \
     vendor/nubia/nx531j/proprietary/lib/hw/audio.primary.msm8996.so:system/lib/hw/audio.primary.msm8996.so \
     vendor/nubia/nx531j/proprietary/lib/hw/camera.msm8996.so:system/lib/hw/camera.msm8996.so \
-    vendor/nubia/nx531j/proprietary/lib/hw/hwcomposer.msm8996.so:system/lib/hw/hwcomposer.msm8996.so \
     vendor/nubia/nx531j/proprietary/lib/libFNVfbEngineHAL.so:system/lib/libFNVfbEngineHAL.so \
     vendor/nubia/nx531j/proprietary/lib/libImageAlogrithm.so:system/lib/libImageAlogrithm.so \
     vendor/nubia/nx531j/proprietary/lib/libmm-qcamera.so:system/lib/libmm-qcamera.so \
@@ -137,12 +136,9 @@ PRODUCT_COPY_FILES += \
     vendor/nubia/nx531j/proprietary/lib/libnubia_effect.so:system/lib/libnubia_effect.so \
     vendor/nubia/nx531j/proprietary/lib/libnubiajpeg.so:system/lib/libnubiajpeg.so \
     vendor/nubia/nx531j/proprietary/lib/libqomx_core.so:system/lib/libqomx_core.so \
-    vendor/nubia/nx531j/proprietary/lib/libsdmcore.so:system/lib/libsdmcore.so \
-    vendor/nubia/nx531j/proprietary/lib/libsdmutils.so:system/lib/libsdmutils.so \
     vendor/nubia/nx531j/proprietary/lib/modules/qca_cld/qca_cld_wlan.ko:system/lib/modules/qca_cld/qca_cld_wlan.ko \
     vendor/nubia/nx531j/proprietary/lib64/hw/audio.primary.msm8996.so:system/lib64/hw/audio.primary.msm8996.so \
     vendor/nubia/nx531j/proprietary/lib64/hw/fingerprint.msm8996.so:system/lib64/hw/fingerprint.msm8996.so \
-    vendor/nubia/nx531j/proprietary/lib64/hw/hwcomposer.msm8996.so:system/lib64/hw/hwcomposer.msm8996.so \
     vendor/nubia/nx531j/proprietary/lib64/hw/sensors.oem.so:system/lib64/hw/sensors.oem.so \
     vendor/nubia/nx531j/proprietary/lib64/libAlgorithm.so:system/lib64/libAlgorithm.so \
     vendor/nubia/nx531j/proprietary/lib64/libBuffer.so:system/lib64/libBuffer.so \
@@ -163,8 +159,6 @@ PRODUCT_COPY_FILES += \
     vendor/nubia/nx531j/proprietary/lib64/libpanoramahelper.so:system/lib64/libpanoramahelper.so \
     vendor/nubia/nx531j/proprietary/lib64/libproductinfo.so:system/lib64/libproductinfo.so \
     vendor/nubia/nx531j/proprietary/lib64/libqti-iop.so:system/lib64/libqti-iop.so \
-    vendor/nubia/nx531j/proprietary/lib64/libsdmcore.so:system/lib64/libsdmcore.so \
-    vendor/nubia/nx531j/proprietary/lib64/libsdmutils.so:system/lib64/libsdmutils.so \
     vendor/nubia/nx531j/proprietary/lib64/libswresample.so:system/lib64/libswresample.so \
     vendor/nubia/nx531j/proprietary/lib64/libswscale.so:system/lib64/libswscale.so \
     vendor/nubia/nx531j/proprietary/vendor/bin/pd-mapper:system/vendor/bin/pd-mapper \
